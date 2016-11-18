246877b15f0824ccc7eb5671cef2f348f89acf4f
Maggie Wiseman <maggie.wiseman@gmail.com>
2016-11-18 09:21:28 -0500

    added negative messages and restyled button

--- js/main.js
+++ js/main.js
@@ -1,25 +1,34 @@
 
 
-var messsageList = ["It is certain", 
-                    "It is decidedly so.", 
-                    "Without a doubt", 
-                    "Yes, definitely", 
-                    "You may rely on it.", 
-                    "As I see it, yes", 
-                    "Most likely.", 
-                    "Outlook good."];
+var messsageList = ["It is certain",
+"It is decidedly so",
+"Without a doubt",
+"Yes, definitely",
+"You may rely on it",
+"As I see it, yes",
+"Most likely",
+"Outlook good",
+"Yes",
+"Signs point to yes",
+"Reply hazy try again",
+"Ask again later",
+"Better not tell you now",
+"Cannot predict now",
+"Concentrate and ask again",
+"Don't count on it",
+"My reply is no",
+"My sources say no",
+"Outlook not so good",
+"Very doubtful"]
 //computes a random number so a random message from array is chosen.  
 
 var getMessage = function() {
    'use strict';
   console.log("in getMessage function about to call math");
-    var index = Math.floor((Math.random() * 10));
+    var index = Math.floor((Math.random() * 20));
     console.log(index);
   
-    //make sure random number is 7 or less because array only has 8 messages
-    if (index > 7) {
-      index = index - 2;
-    } 
+   
   
   //change message in DOM
   $("#triangle").fadeOut(1).fadeIn(2000, "swing");
